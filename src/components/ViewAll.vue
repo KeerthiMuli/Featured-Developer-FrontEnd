531418f7a18601c3c93e2794be9cdba2aaee1695
Rasagna0409 <77762529+Rasagna0409@users.noreply.github.com>
2021-10-15 11:05:09 -0500

    Added view all page

---
+++ src/components/ViewAll.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="page-content page-container" id="page-content">
+      <h1>Featured Developers</h1>
+       <div class="addButton">
+      
+      <!--    <font-awesome-icon color="blue" icon="plus" /> -->
+        
+    </div>
+    <div class="container mt-3">
+      <div class="col mb-4">
+        <div class="card auto">
+          <!-- <img src="..." class="card-img-top" alt="..."> -->
+          <div class="card-body">
+            <img src="../assets/image.png" alt="Avatar" class="avatar">
+            <h5 class="card-title">Person1</h5>
+            <p class="card-text">Role</p>
+            <p class="card-text">Skills</p>
+            <p class="card-text">link</p>
+            <p class="card-text">Goal</p>
+            <p class="card-text">Attainment</p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col mb-4">
+        <div class="card h-100">
+          <!-- <img src="..." class="card-img-top" alt="..."> -->
+          <div class="card-body">
+            <img src="../assets/image.png" alt="Avatar" class="avatar">
+            <h5 class="card-title">Person2</h5>
+            <p class="card-text">Role</p>
+            <p class="card-text">Skills</p>
+            <p class="card-text">link</p>
+            <p class="card-text">Goal</p>
+            <p class="card-text">Attainment</p>
+          </div>
+        </div>
+      </div>
+      <div class="col mb-4">
+        <div class="card h-100">
+          <!-- <img src="..." class="card-img-top" alt="..."> -->
+          <div class="card-body">
+            <img src="../assets/image.png" alt="Avatar" class="avatar">
+            <h5 class="card-title">Person3</h5>
+            <p class="card-text">Role</p>
+            <p class="card-text">Skills</p>
+            <p class="card-text">link</p>
+            <p class="card-text">Goal</p>
+            <p class="card-text">Attainment</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <a href="#/home">Home</a>
+  </div>
+</template>
+<script>
+export default {
+  name: "ViewAll",
+};
+</script>
+<style>
+.h1 {
+  text-align: center;
+  color: black;
+  top: 10px;
+  padding-left: 20px;
+  margin-bottom: 10px;
+}
+.header {
+  height: 90px;
+  width: 100%;
+  border: 1px solid #4caf50;
+  background-color: #006747;
+}
+p { text-align: initial;}
+</style>
