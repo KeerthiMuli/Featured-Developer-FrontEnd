ec48332cf2a519b36d42dd05af5a14d475eff5f5
Rasagna0409 <77762529+Rasagna0409@users.noreply.github.com>
2021-11-03 08:50:26 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,29 +2,14 @@ const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
 
-var cors = require('cors');
-
 const app = express()
-
-
-axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-
-
 
 //here we are configuring dist to serve app files
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
-app.use(cors());
 
 // this * route is to serve project on different page routes except root `/`
 app.get(/.*/, function(req, res) {
     res.sendFile(path.join(__dirname, '/dist/index.html'))
-})
-
-app.get('/developers', (req, res) => {
-    res.set('Access-Control-Allow-Origin', 'http://localhost:8080');
-    res.send({
-        developers: "The way to get started is to quit talking and begin doing."
-    });
 })
 
 const port = process.env.PORT || 8080
